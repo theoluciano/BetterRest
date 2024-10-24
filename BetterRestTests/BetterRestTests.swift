f861544de38f3fc7d4207e6eb5ffc4673d89fe64
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2024-10-24 16:35:37 -0400

    Initial Commit

---
+++ BetterRestTests/BetterRestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestTests.swift
+//  BetterRestTests
+//
+//  Created by Theo Luciano on 10/24/24.
+//
+
+import Testing
+@testable import BetterRest
+
+struct BetterRestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
